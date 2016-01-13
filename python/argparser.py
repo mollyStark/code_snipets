4e0406a7550eef4cc7048d48e67077aa4da80119
mollywang <mollywang@anjuke.com>
2016-01-13 10:32:58 +0800

    python argparser

---
+++ python/argparser.py
@@ -0,0 +1,10 @@
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-x", "--xarg", required=True, help="Function of this argument")
+ap.add_argument("-y", "--optional", help="Function of this argument")
+args = vars(ap.parse_args())
+
+# get the argument
+xarg = args["xarg"]
+opt = args["optional"]
